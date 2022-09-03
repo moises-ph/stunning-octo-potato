02f8eb8d23e4c99499fc5dd92a3d11186cac444b
moisesph <mois.mp8@gmail.com>
2022-09-03 13:34:04 -0500

    Back 1

---
+++ BackEnd/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+
+
+
+
+app.listen(PORT, (req, res)=>{
+  console.log(`Server is running on port ${PORT}`);
+});
