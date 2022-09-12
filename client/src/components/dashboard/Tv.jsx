e1e3ea55ee9367b7bb6285e166db671661017efb
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-12 14:09:33 -0500

    estilos series

--- client/src/components/dashboard/Tv.jsx
+++ client/src/components/dashboard/Tv.jsx
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from "react";
+import style from '../../styles/tv.module.css'
 import axios from "axios";
 
 function Tv() {
-  const [serie, setSerie] = useState(<></>);
+  const [type, setType] = useState("popular");
+  const [movie, setMovie] = useState(<></>);
   const [index, setIndex] = useState(1);
-  const theSerie = (prop) => {
+  const theMovie = (prop) => {
     const results = prop.data.results;
-    setSerie(
+    setMovie(
       results.map((res, id) => {
         return (
-          <div key={id}>
-            <h3>{res.name}</h3>
+          <div className={style.movie} key={id}>
+            <div className={style.contTitle}>
+              <h3 className={style.title}>{res.name}</h3>
+            </div>
             <img
+              className={style.img}
               src={`https://www.themoviedb.org/t/p/w220_and_h330_face${res.backdrop_path}`}
             />
-            <p>
+            {/*<p>
               {res.overview.length > 0
                 ? res.overview
                 : "no description aviable"}
-            </p>
+            </p>*/}
           </div>
         );
       })
@@ -28,45 +33,119 @@ function Tv() {
   useEffect(() => {
     axios
       .get(
-        `https://api.themoviedb.org/3/tv/popular?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=${index}`
+        `https://api.themoviedb.org/3/tv/${type}?api_key=5433a58ed58a7253f675b66bb885524d&language=en-US&page=${index}`
       )
       .then((res) => {
         console.log(res);
-        theSerie(res);
+        theMovie(res);
       });
-  }, [index]);
+  }, [index, type]);
   return (
     <>
-      <h1>Series</h1>
-      <button
-        onClick={() => {
-          setIndex(1);
-        }}
-      >
-        Inicio
-      </button>
-      <button
-        onClick={() => {
-          setIndex(index > 1 ? index - 1 : index);
-        }}
-      >
-        Anteriores
-      </button>
-      <button
-        onClick={() => {
-          setIndex(index < 500 ? index + 1 : index);
-        }}
-      >
-        Siguientes
-      </button>
-      <button
-        onClick={() => {
-          setIndex(500);
-        }}
-      >
-        Final
-      </button>
-      <div>{serie}</div>
+      <div className={style.body}>
+        <nav className={style.nav}>
+          <h1>Peliculas</h1>
+          <div className={style.nbotones}>
+            <button
+              className={style.sbutton}
+              onClick={() => setType("popular")}
+            >
+              Populares
+            </button>
+            <button
+              className={style.sbutton}
+              onClick={() => setType("top_rated")}
+            >
+              Mejores Ranqueadas
+            </button>
+            <button
+              className={style.sbutton}
+              onClick={() => setType("on_the_air")}
+            >
+              Proximas a estrenarse
+            </button>
+          </div>
+        </nav>
+
+        <div className={style.buttons}>
+          <div className={style.menos}>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(1);
+              }}
+            >
+              Inicio
+            </button>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(index > 1 ? index - 1 : index);
+              }}
+            >
+              Anteriores
+            </button>
+          </div>
+          <div className={style.mas}>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(index < 500 ? index + 1 : index);
+              }}
+            >
+              Siguientes
+            </button>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(500);
+              }}
+            >
+              Final
+            </button>
+          </div>
+        </div>
+        <div className={style.movies}>{movie}</div>
+
+        <div className={style.buttons}>
+          <div className={style.menos}>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(1);
+              }}
+            >
+              Inicio
+            </button>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(index > 1 ? index - 1 : index);
+              }}
+            >
+              Anteriores
+            </button>
+          </div>
+          <div className={style.mas}>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(index < 500 ? index + 1 : index);
+              }}
+            >
+              Siguientes
+            </button>
+            <button
+              className={style.boton}
+              onClick={() => {
+                setIndex(500);
+              }}
+            >
+              Final
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
