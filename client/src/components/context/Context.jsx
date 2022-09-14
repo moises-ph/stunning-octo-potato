a8002be09228a49ad48a62d1b4f2a359856b46a2
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-14 15:20:36 -0500

    contexto

---
+++ client/src/components/context/Context.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { createContext, useState,  } from 'react'
+
+export const ContextLogin = createContext();
+
+export const Context = ({children}) => {
+  const [token, setToken] = useState()
+  return (
+    <ContextLogin.Provider value={{token, setToken}}>
+      {children}
+    </ContextLogin.Provider>
+  )
+}
+
+export default Context
