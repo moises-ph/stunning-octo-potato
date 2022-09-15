ef2bdbb517196db25f92112a79c9ee62bcef6631
Ethiem Guerrero <ethiemalex03@gmail.com>
2022-09-15 16:30:59 -0500

    consumo de api de libros

--- client/src/components/dashboard/Libros.jsx
+++ client/src/components/dashboard/Libros.jsx
@@ -1,15 +1,51 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 
 function Libros() {
-  useEffect(()=>{
-    axios.get('https://www.etnassoft.com/api/v1/get/?category=libros_programacion&criteria=most_viewed').then(res=>{
-      console.log(res);
-    })
-  }, [])
+  const [content, setContent] = useState();
+  const [query, setQuery] = useState("the+lord+of+the+rings")
+  const book = useRef();
+
+  const theQuery = () => {
+    setQuery(book.current.value.split(' ').join('+'))
+  }
+
+  const theContent = (props) => {
+    if(props.data.numFound !== 0){
+    setContent(
+      props.data.docs.map((info, index) => {
+        return (
+          <div key={index}>
+            <h2>{info.title}</h2>
+            <h3>{info.author_name ? info.author_name : 'no hay autor disponible'}</h3>
+          </div>
+        );
+      })
+    );
+    }
+    else{
+      setContent(
+        <h2>Por favor especificar mas la busqueda</h2>
+      )
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://openlibrary.org/search.json?q=${query}`)
+      .then((res) => {
+        console.log(res);
+        theContent(res);
+      })
+      .then();
+  }, [query]);
   return (
-    <div>Libros</div>
-  )
+    <>
+      <input type="text" ref={book}/>
+      <button onClick={theQuery}>Buscar</button>
+      <div>{content}</div>
+    </>
+  );
 }
 
-export default Libros
+export default Libros;
